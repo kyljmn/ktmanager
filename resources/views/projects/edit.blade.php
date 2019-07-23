8afa3e706db710adfc143a0035fcfabb2b6b0995
kyljmn <kylejemino@gmail.com>
2019-07-23 10:07:45 +0800

    First commit

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,20 @@
+<form action="/projects/{{ $project->id }}" method="POST">
+  @method('PATCH')
+  @csrf
+  <div>
+    <h4>Project Title</h4>
+    <input type="text" name="title" value="{{$project->title}}" required>
+  </div>
+  <div>
+    <h4>Project Description</h4>
+    <textarea name="description" placeholder="Write something about your project here" required>{{$project->description}}</textarea>
+  </div>
+  <div>
+    <h4>Deadline</h4>
+    <input type="datetime-local" name="deadline" value="{{$project->deadline}}" required>
+  </div>
+  <br><br>
+  <div>
+    <button type="submit" name="button">Edit project</button>
+  </div>
+</form>
