3fb4b895b9f8673997c8fe902672c5b203449b6f
kyljmn <kylejemino@gmail.com>
2019-07-23 10:20:54 +0800

    .gitignore

--- resources/views/projects/index.blade.php
+++
@@ -1,9 +0,0 @@
-<ul>
-
-  @foreach ($projects as $project)
-    <li>
-      <p><a href="/projects/{{$project->id}}">{{ $project->title }}</a></p>
-      <p>{{ $project->deadline }}</p>
-    </li>
-  @endforeach
-</ul>
