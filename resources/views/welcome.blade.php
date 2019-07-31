b43196d8da3d7af9066931a797228e207ad9fd43
kyljmn <kylejemino@gmail.com>
2019-07-31 18:27:27 +0800

    edited views/welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,9 @@
 @endsection
 @section('content')
 <div class="ui one column center aligned stackable page grid" style="margin:30vh;">
-   <div class="column six wide">
-         <div class="ui center aligned huge header">
-           <h1>ToDone</h1>
-         </div>
-         <p class="ui small header">From to do to done real quick.</p>
+   <div class="column eight wide">
+          <h1>ToDone</h1>
+         <p class="ui small header">From "to do" to "done" real quick.</p>
   </div>
 </div>
 
