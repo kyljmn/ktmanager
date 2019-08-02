87fb46384f80b8c45f30aa052bc78afbbd69357e
kyljmn <kylejemino@gmail.com>
2019-08-02 22:44:17 +0800

    fixed redirects

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -91,7 +91,7 @@ class ProjectTasksController extends Controller
         $validated = $this->validateTask();
         $validated['deadline'] = Carbon::createFromFormat('F j, Y g:i A', $request->deadline, auth()->user()->timezone)->timezone('UTC');
         $task->update($validated);
-        return redirect(session('links')[2]);
+        Redirect::to($request->request->get('http_referrer'));
     }
 
     /**
